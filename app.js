7dd03e56e641818339a367cc95166059e2c895cf
jedllorente <jedllorente@gmail.com>
2023-07-27 13:31:17 +0800

    accessing object properties

---
+++ app.js
@@ -0,0 +1,10 @@
+let age = 30;
+let userName = "Jed";
+let hobbies = ['Music', 'Programming', 'Riding'];
+let job = {
+    title: 'Web Developer',
+    place: 'Singapore',
+    salary: 3000
+};
+
+alert(hobbies[0]);
